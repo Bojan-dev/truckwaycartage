e224f56b3503c83a9b34d3eb758c44b857ee6d77
Bojan Vreco <bojanvreco069@gmail.com>
2022-06-20 20:20:08 +0200

    Animation removed

--- script.js
+++ script.js
@@ -24,10 +24,6 @@ mainHeaderSpans.forEach((headerSpan) => {
 
 const mainIllustration = document.querySelector('.main--img');
 
-window.addEventListener('load', () => {
-  mainIllustration.style.transform = 'translateX(0)';
-});
-
 //FORM SUBMIT:
 const firstForm = document.getElementById('apply-today--form-first');
 const secondForm = document.getElementById('apply-today--form-second');
