76f8e3900f23beecfb4013ee89001b3fae2ec09e
Bojan-dev <bojanvreco069@gmail.com>
2023-05-25 20:09:12 +0200

    Form finished, few additional fixes

--- script.js
+++ script.js
@@ -23,17 +23,49 @@ mainHeaderSpans.forEach((headerSpan) => {
 });
 
 const mainIllustration = document.querySelector('.main--img');
+const observerElms = document.querySelectorAll('[data-observe]');
+
+//INTERSECTION OBSERVER:
+const observerFun = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const elClasses = entry.target.dataset.observe.split(' ');
+      elClasses.forEach((cl) => entry.target.classList.remove(cl));
+    }
+  });
+};
+
+const observerOptions = {
+  threshold: 0.75,
+};
+
+const observer = new IntersectionObserver(observerFun, observerOptions);
+
+observerElms.forEach((el) => observer.observe(el));
 
 //FORM SUBMIT:
-const firstForm = document.getElementById('apply-today--form-first');
-const secondForm = document.getElementById('apply-today--form-second');
-const ageSelect = document.getElementById('choose-age');
-const cdlSelect = document.getElementById('choose-cdl');
-const experienceSelect = document.getElementById('choose-experience');
-const selectArr = [ageSelect, cdlSelect, experienceSelect];
+const form = document.getElementById('form');
+const firstFormBtn = form.querySelector('button[type="button"]');
+const firstFormWrapper = document.querySelector('.first-form--wrapper');
+const secondFormWrapper = document.querySelector('.second-form--wrapper');
+// prettier-ignore
+const selectArr = [document.getElementById('choose-age'), document.getElementById('choose-cdl'), document.getElementById('choose-experience')];
 const formSteps = document.querySelector('.apply-today--form-steps')
   .children[0];
+
+const nameInput = document.getElementById('enter-name');
+const phoneInput = document.getElementById('enter-number');
+const emailInput = document.getElementById('enter-email');
+const inputArr = [nameInput, phoneInput, emailInput];
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+const modalCloseBtn = document.querySelector('.btn--modal');
 const backArrow = document.querySelector('.back-arrow');
+const nameRegex = /^[a-zA-Z\s]*$/;
+const phoneRegex =
+  /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/;
+// prettier-ignore
+const emailRegex =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 const handleVisibility = (el, visible = true) => {
   if (visible) {
@@ -58,103 +90,29 @@ const handleFieldError = (field) => {
   handleVisibility(document.getElementById(`${field.id}-error`));
 };
 
-const formsData = {
-  firstform: false,
-  firstFormData: {},
-  secondForm: false,
-  secondFormData: {},
-};
-
-firstForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-
+firstFormBtn.addEventListener('click', () => {
   let handleFirstStepErrors = 0;
 
-  if (ageSelect.selectedIndex === 0) {
-    handleFieldError(ageSelect);
+  const emptySelect = selectArr.filter((select) => select.selectedIndex === 0);
+
+  emptySelect.forEach((select) => {
+    handleFieldError(select);
     handleFirstStepErrors++;
-  }
-  if (cdlSelect.selectedIndex === 0) {
-    handleFieldError(cdlSelect);
-    handleFirstStepErrors++;
-  }
-  if (experienceSelect.selectedIndex === 0) {
-    handleFieldError(experienceSelect);
-    handleFirstStepErrors++;
-  }
+  });
 
   if (handleFirstStepErrors === 0) {
     formSteps.style.width = '100%';
-    handleVisibility(e.target, false);
-    handleVisibility(secondForm);
+    handleVisibility(firstFormWrapper, false);
+    handleVisibility(secondFormWrapper);
     backArrow.removeAttribute('disabled');
-    formsData.firstform = true;
-    formsData.firstFormData.age = ageSelect.value;
-    formsData.firstFormData.cdl = cdlSelect.value;
-    formsData.firstFormData.experience = experienceSelect.value;
   }
 });
 
-const nameRegex = /^[a-zA-Z\s]*$/;
-const phoneRegex =
-  /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/;
-// prettier-ignore
-const emailRegex =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+function closeModalHandler() {
+  location.reload();
+}
 
-const nameInput = document.getElementById('enter-name');
-const phoneInput = document.getElementById('enter-number');
-const emailInput = document.getElementById('enter-email');
-const inputArr = [nameInput, phoneInput, emailInput];
-const overlay = document.querySelector('.overlay');
-const modal = document.querySelector('.modal');
-
-inputArr.forEach((inputEl) => {
-  inputEl.addEventListener('focus', (e) => {
-    e.target.style.borderColor = 'var(--gray-clr)';
-    handleVisibility(document.getElementById(`${e.target.id}-error`), false);
-  });
-});
-
-const sendData = async () => {
-  overlay.classList.remove('hidden');
-  modal.innerHTML = `<img class="loading" src="imgs/loading-icon.png" alt="Loading..." />`;
-  try {
-    const request = await fetch(
-      `https://guerro-49b06-default-rtdb.firebaseio.com/applications.json`,
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          name: formsData.secondFormData.name,
-          phone: formsData.secondFormData.phone,
-          email: formsData.secondFormData.email,
-          age: formsData.firstFormData.age,
-          cdl: formsData.firstFormData.cdl,
-          experience: formsData.firstFormData.experience,
-        }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    );
-
-    if (!request.ok) throw new Error(`Something's wrong, try again later!`);
-
-    modal.innerHTML = ``;
-    modal.insertAdjacentHTML(
-      'afterbegin',
-      `<h3>Application is successfully submitted!</h3>
-    <button class="bkg--red btn--modal">Close</button>`
-    );
-
-    const modalCloseBtn = document.querySelector('.btn--modal');
-
-    modalCloseBtn.addEventListener('click', closeModalHandler);
-  } catch (err) {
-    modal.innerHTML = err.message;
-  }
-};
-
-secondForm.addEventListener('submit', (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   let handleSecondStepErrors = 0;
@@ -175,43 +133,43 @@ secondForm.addEventListener('submit', (e) => {
   }
 
   if (handleSecondStepErrors === 0) {
-    formsData.secondForm = true;
-    formsData.secondFormData.name = nameInput.value;
-    formsData.secondFormData.phone = phoneInput.value;
-    formsData.secondFormData.email = emailInput.value;
-    sendData();
+    overlay.classList.remove('hidden');
+    setTimeout(() => {
+      form.submit();
+    }, 1000);
   }
+});
+
+modalCloseBtn.addEventListener('click', closeModalHandler);
+
+inputArr.forEach((inputEl) => {
+  inputEl.addEventListener('focus', (e) => {
+    e.target.style.borderColor = 'var(--gray-clr)';
+    handleVisibility(document.getElementById(`${e.target.id}-error`), false);
+  });
 });
 
 backArrow.addEventListener('click', (e) => {
-  if (formsData.firstform === true) {
-    handleVisibility(secondForm, false);
-    handleVisibility(firstForm);
-    formSteps.style.width = '50%';
-    e.target.setAttribute('disabled', '');
-  }
+  handleVisibility(secondFormWrapper, false);
+  handleVisibility(firstFormWrapper);
+  formSteps.style.width = '50%';
+  e.target.setAttribute('disabled', '');
 });
-
-function closeModalHandler() {
-  location.reload();
-}
 
 const calculateCoordsDiff = (start, end) => {
   return end - start;
 };
-
-//FIRST FORM TOUCH EVENTS
 
 const touchCoordsFirstForm = {
   startingCoord: 0,
   endingCoord: 0,
 };
 
-firstForm.addEventListener('touchstart', (e) => {
+firstFormWrapper.addEventListener('touchstart', (e) => {
   touchCoordsFirstForm.startingCoord = e.changedTouches[0].clientX;
 });
 
-firstForm.addEventListener('touchend', (e) => {
+firstFormWrapper.addEventListener('touchend', (e) => {
   touchCoordsFirstForm.endingCoord = e.changedTouches[0].clientX;
 
   const coordsDifference = calculateCoordsDiff(
@@ -219,8 +177,8 @@ firstForm.addEventListener('touchend', (e) => {
     touchCoordsFirstForm.endingCoord
   );
 
-  if (-coordsDifference > firstForm.clientWidth / 2)
-    firstForm.dispatchEvent(new Event('submit'));
+  if (-coordsDifference > firstFormWrapper.clientWidth / 2)
+    firstFormBtn.dispatchEvent(new Event('click'));
 });
 
 //SECOND FORM TOUCH EVENTS
@@ -230,11 +188,11 @@ const touchCoordsSecondForm = {
   endingCoord: 0,
 };
 
-secondForm.addEventListener('touchstart', (e) => {
+secondFormWrapper.addEventListener('touchstart', (e) => {
   touchCoordsSecondForm.startingCoord = e.changedTouches[0].clientX;
 });
 
-secondForm.addEventListener('touchend', (e) => {
+secondFormWrapper.addEventListener('touchend', (e) => {
   touchCoordsSecondForm.endingCoord = e.changedTouches[0].clientX;
 
   const coordsDifference = calculateCoordsDiff(
@@ -242,9 +200,9 @@ secondForm.addEventListener('touchend', (e) => {
     touchCoordsSecondForm.endingCoord
   );
 
-  if (-coordsDifference > secondForm.clientWidth / 2)
-    secondForm.dispatchEvent(new Event('submit'));
+  if (-coordsDifference > secondFormWrapper.clientWidth / 2)
+    form.dispatchEvent(new Event('submit'));
 
-  if (coordsDifference > secondForm.clientWidth / 2)
+  if (coordsDifference > secondFormWrapper.clientWidth / 2)
     backArrow.dispatchEvent(new Event('click'));
 });
